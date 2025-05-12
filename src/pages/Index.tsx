f585c8a0189c5148a792337535f83ee485d6db8a
iloveprompt <ilovepromptsaas@gmail.com>
2025-05-12 12:22:52 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -131,12 +131,10 @@ const Index = () => {
                     <code className="text-aquaGreen">10. Utilize ESLint e Prettier com configurações de projeto ...</code>
                     <br />
                     <br />
-                    <div className="flex flex-wrap justify-center gap-4 items-center">
                     <pre className="text-[12px] font-mono whitespace-pre-wrap">
-                    <code className="text-aquaGreen">Use em sua ferramenta de vibe coding preferida. </code>
-                    <br />
+                    <code className="text-aquaGreen">Use em sua ferramenta de vibe coding preferida.</code>
                     </pre>
-                      <br />
+                    <div className="flex flex-wrap justify-center gap-4 items-center">
                       <a href="https://lovable.dev/" target="_blank" rel="noopener noreferrer">
                       <img src="/lovable-uploads/1afc2e55-681b-40b7-9384-7675460432fe.png" alt="Lovable" className="h-5 w-auto" />
                       </a>
